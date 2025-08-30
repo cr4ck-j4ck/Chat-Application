6d2956d06a4dfff5b74d2db862c17ff301e8e716
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-08-30 13:05:50 +0530

    feat: integrate Socket.IO client in frontend
    Added Socket.IO client in App.tsx to establish WebSocket connection
    with backend using userName as auth token. Updated package.json to
    include socket.io-client dependency.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 import AppRoutes from "./Routes/routes";
+import useUserStore from "./Store/user.store";
+import { io } from "socket.io-client";
 
 function App() {
+  const user = useUserStore((state) => state.user);
+  if (user) {
+    const socket = io("http://localhost:3000", {
+      reconnectionDelayMax: 10000,
+      auth: {
+        token:user.userName
+      },
+    });
+  }
   return <AppRoutes />;
 }
 
