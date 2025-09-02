81eda4688837611318176eeaed66ea83e763a61d
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-02 20:14:23 +0530

    feat: implement friend request management functionality in user controller

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -2,11 +2,13 @@ import User from "../models/user.model";
 import { Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { Server } from "socket.io";
+import { IpopulatedUser } from "../Types/interface.";
 
 export async function createUser(req: Request, res: Response) {
-  console.log("Requestt recieved", req.body);
   if (req.body && req.body.userData.email) {
-    const { email, password, firstName, lastName, userName } = req.body.userData;
+    const { email, password, firstName, lastName, userName } =
+      req.body.userData;
     const existingUser = await User.findOne({ email: email });
     if (existingUser) {
       return res.status(400).send("User Already Exists!!.. You have to login");
@@ -18,7 +20,7 @@ export async function createUser(req: Request, res: Response) {
         password: hashedPassword,
         firstName,
         lastName,
-        userName
+        userName,
       })
     ).toObject();
     const { password: pwd, ...filteredUser } = newUser;
@@ -56,7 +58,7 @@ export async function loginUser(req: Request, res: Response) {
     return res.status(401).send("Wrong password!");
   }
   const token = jwt.sign(
-    { userId: existingUser._id ,userName:existingUser.userName},
+    { userId: existingUser._id, userName: existingUser.userName },
     process.env.JWT_SECRET!,
     { expiresIn: "6d" }
   );
@@ -72,9 +74,12 @@ export async function loginUser(req: Request, res: Response) {
   });
   res.send("User Loggined Successfully!!");
 }
-export async function sendUser(req: Request, res: Response) {}
 
-export async function verifyUser(req: Request, res: Response) {
+export async function verifyUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
     const { token } = req.cookies;
     if (!token) {
@@ -82,27 +87,150 @@ export async function verifyUser(req: Request, res: Response) {
     }
     const isVerified = jwt.verify(token, process.env.JWT_SECRET!) as {
       userId: string;
+      userName: string;
     };
     if (!isVerified) {
       res.status(503).send("Error Occurred!! While verifying your Code");
     }
-    req.user = { userId: isVerified.userId };
-    return res.send("verifying..");
+    const existingUser = await User.findById(isVerified.userId).lean();
+    if (!existingUser) {
+      return res.status(401).send("User Doesn't Exists");
+    }
+    if (req.originalUrl === "/user/auth/status") {
+      const { password, ...responseObject } = existingUser;
+      return res.json(responseObject);
+    }
+    req.user = { userId: isVerified.userId, userName: isVerified.userName };
+    next();
   } catch (err) {
-    console.log("kya kar rha hai be error aa gayi catch karliya..", err);
-    return res.status(500).send("Lode lag gaye");
+    console.log(
+      "kya kar rha hai be error aa gayi catch karliya.. lekin yeh catch hua hai cathc mein",
+      err
+    );
+    return res.status(500).send("lag gaye");
   }
 }
 
 export async function checkUniqueUsername(req: Request, res: Response) {
   const { userName } = req.query;
-  if(!userName){
+  if (!userName) {
     return res.status(400).send("UserName required!!");
   }
-  const existingUserWithUsername = await User.findOne({userName});
-  console.log("UserName dekh existingUser se",existingUserWithUsername);
-  if(!existingUserWithUsername){
-    return res.send(true)
+  const existingUserWithUsername = await User.findOne({ userName });
+  console.log("UserName dekh existingUser se", existingUserWithUsername);
+  if (!existingUserWithUsername) {
+    return res.send(true);
   }
   res.send(false);
+}
+
+export async function addUserNameToFriendList(req: Request, res: Response) {
+  const { toUserName } = req.body;
+  if (!toUserName) {
+    return res.status(400).send("Please provide Enough Data!");
+  }
+
+  const requestSentToTheUser = await User.findOneAndUpdate(
+    { userName: toUserName },
+    { $addToSet: { friendsRequests: req.user.userId } }, // prevents duplicates
+    { new: true }
+  );
+  if (!requestSentToTheUser) {
+    return res.status(400).send("User Doesn't Exists!!");
+  }
+  const io = req.app.get("io") as Server;
+  io.to(toUserName).emit("receivedRequest", req.user.userName);
+  res.send("Request Sent");
+}
+
+export async function friendsData(req: Request, res: Response) {
+  const user = req.user;
+  if (!user) {
+    return res.status(400).send("Please Login First!!");
+  }
+  const friendListData = await User.findById(user.userId, { _id: false })
+    .populate<{ friendsRequests: IpopulatedUser[] }>({
+      path: "friendsRequests",
+      select: "firstName lastName userName avatar",
+    })
+    .populate<{ friends: IpopulatedUser[] }>({
+      path: "friends",
+      select: "firstName lastName userName avatar",
+    })
+    .lean();
+  res.json({
+    friendRequestList: friendListData?.friendsRequests,
+    friends: friendListData?.friends,
+  });
+}
+
+export async function acceptFriendRequest(req: Request, res: Response) {
+  try {
+    const { userId: acceptedFriendId } = req.body;
+
+    if (!acceptedFriendId) {
+      return res.status(400).send("Please provide the user ID!");
+    }
+
+    await User.findByIdAndUpdate(req.user.userId, {
+      $pull: { friendsRequests: acceptedFriendId },
+      $push: { friends: acceptedFriendId },
+    });
+    const updatedRequestUser= await User.findByIdAndUpdate(acceptedFriendId, {
+      $addToSet: { friends: req.user.userId }      
+    },{new:true}).lean();
+    if (updatedRequestUser) {
+      const responseJson = {
+        firstName:updatedRequestUser.firstName,
+        lastName:updatedRequestUser.lastName,
+        userName:updatedRequestUser.userName,
+        _id:updatedRequestUser._id,
+        online:updatedRequestUser.status
+      }
+      const socket = req.app.get("io") as Server;
+      socket
+        .to(updatedRequestUser.userName)
+        .emit("acceptedFriendRequest", req.user.userName);
+      return res.status(200).json(responseJson);
+    }
+    res.status(500).send("Some Error Ocurred!!");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+}
+
+export async function rejectFriendRequest(req: Request, res: Response) {
+  const { userId: requestedFriendId } = req.query;
+  await User.findByIdAndUpdate(req.user.userId, {
+    $pull: { friendsRequests: requestedFriendId },
+  });
+  res.send("Rejected SuccessFully!!");
+}
+
+export async function removeFriend(req: Request, res: Response) {
+  const { friendId } = req.query;
+  if (!friendId) {
+    return res
+      .status(403)
+      .send("Please Provide the Friend ID you want to remove");
+  }
+  const removerFriend = await User.findByIdAndUpdate(
+    req.user.userId,
+    {
+      $pull: { friends: friendId },
+    },
+    { new: true }
+  );
+  const removedFriend = await User.findByIdAndUpdate(
+    friendId,
+    {
+      $pull: { friends: req.user.userId },
+    },
+    { new: true }
+  );
+  console.log("ho gaye remove!!");
+  console.log(removerFriend);
+  console.log(removedFriend);
+  res.send("Friend Removed SuccessFully!!");
 }
