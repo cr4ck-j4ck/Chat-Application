665dc6881319f5daf4415a60a76be1c4700ec7b3
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-02 20:09:06 +0530

    Move user fetch logic from PrivateRoutes to App.tsx. Add fetchResult state to global store to track user data fetch status. Update PrivateRoutes to use global store's fetchResult for navigation.

--- Frontend/src/Store/global.store.ts
+++ Frontend/src/Store/global.store.ts
@@ -1,13 +1,19 @@
 import { Socket } from "socket.io-client";
 import { create } from "zustand";
-
+type Tfetch = "error" | "success" | "processing" | "resting";
 export interface IglobalStore {
   socket:Socket | null,
+  fetchResult:Tfetch;
+  setFetchResult:(toUpdate:Tfetch)=> void;
   setSocket: (toUpdate : Socket|null)=> void;
 }
 
 const useGlobalStore = create<IglobalStore>((set) => ({
   socket : null,
+  fetchResult:"resting",
+  setFetchResult(toUpdate){
+    set({fetchResult:toUpdate})
+  },
   setSocket(toUpdate) {
     set({socket:toUpdate});
   },
--- Frontend/src/components/PrivateRoutes.tsx
+++ Frontend/src/components/PrivateRoutes.tsx
@@ -1,36 +1,16 @@
 import useUserStore from "@/Store/user.store";
-import { useShallow } from "zustand/react/shallow";
-import { getUser } from "@/Services/user.api";
-import { useState } from "react";
+import useGlobalStore from "@/Store/global.store";
 import { Navigate } from "react-router-dom";
-
-type Tfetch = "error" | "success" | "processing.." | "resting..";
 
 const PrivateRoutes = ({
   children,
 }: {
   children: React.ReactNode;
 }): React.JSX.Element => {
-  const { user, setUser } = useUserStore(
-    useShallow((state) => ({ user: state.user, setUser: state.setUser }))
-  )
-  const [fetchResult, setFetchResult] = useState<Tfetch>("resting..");
-  if (!user) {
-    getUser()
-      .then((result) => {
-        const [err, user] = result;
-        if (err) {
-            setFetchResult("error");
-            return console.log(err);
-        }
-        setUser(user!);
-      })
-      .catch((err) => {
-        console.log("Error Occurred", err);
-        setFetchResult("error");
-      })
-  }
-  if (user && fetchResult == "resting..") {
+  const fetchResult = useGlobalStore(state => state.fetchResult);
+  const  user = useUserStore(state => state.user  )
+  
+  if (user && fetchResult == "resting") {
     return <>{children}</>;
   }else if(fetchResult == "error"){
     return <Navigate to={"/auth"}/>
