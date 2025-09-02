1da25ac6ebb23417a7e566829eafa75b1b1ac93e
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-02 20:08:33 +0530

    Update socket in App.tsx to handle acceptedFriendRequest event and display success toast when a friend request is accepted.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,29 +5,52 @@ import useUserStore from "./Store/user.store";
 import { io } from "socket.io-client";
 import useGlobalStore from "./Store/global.store";
 import { toast } from "sonner";
+import { useShallow } from "zustand/react/shallow";
+import { getUser } from "./Services/user.api";
 
 function App() {
-  const user = useUserStore((state) => state.user);
-  const setSocket = useGlobalStore(state => state.setSocket)
-
-  useEffect(()=>{
+  const { user, setUser } = useUserStore(
+    useShallow((state) => ({ user: state.user, setUser: state.setUser }))
+  );
+  const setSocket = useGlobalStore((state) => state.setSocket);
+  const  setFetchResult= useGlobalStore(state =>
+      state.setFetchResult
+  );
+  useEffect(() => {
+    if (!user) {
+      getUser()
+        .then((result) => {
+          const [err, user] = result;
+          if (err) {
+            setFetchResult("error");
+            return console.log(err);
+          }
+          setUser(user!);
+        })
+        .catch((err) => {
+          console.log("Error Occurred", err);
+          setFetchResult("error");
+        });
+    }
+  }, []);
+  useEffect(() => {
     if (user) {
-    const socket = io("http://localhost:3000", {
-      reconnectionDelayMax: 10000,
-      auth: {
-        token:user.userName
-      },
-      withCredentials:true
-    });
-    setSocket(socket);
-    socket.on("receivedRequest",(userName)=>{
-      console.log("you got request from ",userName);
-      toast(`You Got Request from ${userName}`);
-    })
-    socket.on('disconnect', () => {console.log("user Disconnected!!")});
-
-  }
-}, [user, setSocket]);
+      const socket = io("http://localhost:3000", {
+        reconnectionDelayMax: 10000,
+        auth: {
+          token: user.userName,
+        },
+        withCredentials: true,
+      });
+      setSocket(socket);
+      socket.on("disconnect", () => {
+        console.log("user Disconnected!!");
+      });
+      socket.on("acceptedFriendRequest",(userName)=>{
+        toast.success(`${userName} accepted Your Request!!`)
+      })
+    }
+  }, [user]);
   return <AppRoutes />;
 }
 
