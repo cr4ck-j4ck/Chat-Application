c917841526e209efeebe4d347944d6ad4cf749c7
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-03 11:37:43 +0530

    Add lastMessage field to chat schema and update timestamps in chat.model.ts

--- Backend/src/models/chat.model.ts
+++ Backend/src/models/chat.model.ts
@@ -1,22 +1,35 @@
-import { Schema, model, Document, Types } from "mongoose";
+import mongoose, { Schema, model, Document, Types, mongo } from "mongoose";
 
 export interface IChat extends Document {
   type: "direct" | "group";
   participants: Types.ObjectId[]; // User references
-  groupName:string;
+  lastMessage: {
+    content: string,
+    senderId: string,
+    timestamp: Date
+  },
   createdAt: Date;
 }
 
 const chatSchema = new Schema<IChat>(
   {
     type: { type: String, enum: ["direct", "group"], default: "direct" },
-    participants: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
-    groupName:{
+    participants: [
+      { type: Schema.Types.ObjectId, ref: "User", required: true },
+    ],
+    lastMessage: {
+      content: {
         type:String,
         required:true
-    }
+      },
+      senderId: {
+        type:String,
+        required:true
+      },
+      timestamp: Date
+    },
   },
-  { timestamps: { createdAt: true, updatedAt: false } }
+  { timestamps: { createdAt: true, updatedAt: true } }
 );
 
 export const Chat = model<IChat>("Chat", chatSchema);
