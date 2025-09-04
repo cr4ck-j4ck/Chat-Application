03b57cae3973e0ea7eb6810543fef446808bc9fb
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-04 16:48:35 +0530

    frontend(store): enhance user store with friends support and response typing

--- Frontend/src/Store/user.store.ts
+++ Frontend/src/Store/user.store.ts
@@ -1,22 +1,59 @@
 import { create } from "zustand";
-
-export interface Iuser {
-  userName:string
+export interface IfriendRequests {
   firstName: string;
   lastName: string;
+  userName: string;
+  _id: string;
+}
+
+export interface Ifriends extends IfriendRequests {
+  avatar?:string;
+  isOnline: boolean;
+  isPinned:boolean;
+  isMuted:boolean;
+  type: "Personal";
+  lastMessage?: string;
+  unreadCount:number;
+  timestamp:string
+}
+export interface IresponseUser {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
   email: string;
+  avatar: string;
+  provider: string;
+  status: string;
+  blockedUsers: [];
+  chats: [];
+  socketId: string;
+  lastSeen: string;
+  createdAt: string;
+  updatedAt: string;
+  friendsRequests: [];
+  friends: Ifriends[];
 }
 
 interface IuserStore {
-  user: Iuser | null;
-  setUser: (toUpdate: Iuser) => void;
+  user: IresponseUser | null;
+  setUser: (toUpdate: IresponseUser) => void;
+  setFriends:(toUpdate:Ifriends|Ifriends[]) => void;
 }
 
-const useUserStore = create<IuserStore>((set) => ({
-  user:null,
+const useUserStore = create<IuserStore>((set,get) => ({
+  user: null,
   setUser(toUpdate) {
     set({ user: toUpdate });
   },
+  setFriends:function(toUpdate){
+    const user = get().user;
+    if(user){
+      set(({user:{...user,
+      friends: toUpdate instanceof Array ? toUpdate : [...user.friends,toUpdate]
+      }}));
+    }
+  }
 }));
 
 export default useUserStore;
