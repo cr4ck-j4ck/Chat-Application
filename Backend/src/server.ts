608ab7d27a93addcfb31c765eda5cf6155aa8fd0
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-05 21:33:01 +0530

    feat: Implement real-time messaging functionality

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -7,12 +7,15 @@ import cors from "cors";
 import jwt from "jsonwebtoken";
 import { Server } from "socket.io";
 import { Request, Response, NextFunction } from "express";
+import { Message } from "./models/message.model";
 import { SocketData } from "./Types/socket";
-import { addUserNameToFriendList } from "./controllers/user.controller";
+
 dotenv.config();
 
 // Routes I2mport
 import UserRouter from "./Routes/user.routes";
+import { type IsentMessage } from "./Types/interface.";
+import { Conversation } from "./models/conversation.model";
 
 const Port = process.env.PORT || 3000;
 const app = express();
@@ -21,7 +24,7 @@ connectDB(process.env.MONGO_URI!);
 // Using Routes
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://gufta-gu.vercel.app"], // specify exact origin
+    origin: ["http://localhost:5173", "https://gufta-gu.vercel.app"], // specify exact origin
     credentials: true, // allow credentials
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -32,10 +35,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookiesParser());
 app.use("/user", UserRouter);
-app.get("/random",(req,res)=>{
-  console.log("han aa gayi");
+app.get("/random", (req, res) => {
   res.redirect("https://www.google.com");
-})
+});
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   res.status(500).json({ message: "Internal Server Error" });
@@ -47,11 +49,11 @@ const server = app.listen(Port, () => {
 
 const io = new Server<any, any, any, SocketData>(server, {
   cors: {
-    origin: ["http://localhost:5173","https://gufta-gu.vercel.app"],
+    origin: ["http://localhost:5173", "https://gufta-gu.vercel.app"],
     credentials: true,
   },
 });
-app.set("io",io);
+app.set("io", io);
 io.use((socket, next) => {
   const token = socket.handshake.headers.cookie;
   if (!token) {
@@ -77,6 +79,48 @@ io.use((socket, next) => {
 });
 
 io.on("connection", (socket) => {
-  socket.join(socket.data.user.userName);
+  socket.join(socket.data.user.userId);
   console.log(`A user Connected w ith userName ${socket.data.user.userName}`);
+  socket.on("send_message", async (data: IsentMessage) => {
+    // Save to DB
+    if (data.participants instanceof Array && !data.conversationId) {
+      return socket.emit("errorOfConvoMsg", "Please Provide conversation id ");
+    }
+  
+    if (!data.conversationId && !(data.participants instanceof Array) && data.userName) {
+      const newConvo = await Conversation.create({
+        participants: [data.senderId, data.participants.userId],
+        type: "direct",
+      });
+      await Message.create({
+        conversationId: newConvo._id,
+        senderId: data.senderId,
+        content: data.content,
+        type: "text",
+      });
+      const socks = await io.in(data.userName).fetchSockets();
+      socks.forEach(s => 
+        s.join(newConvo.id)
+      )
+      socket.join(newConvo.id);
+      return socket.to(newConvo.id).emit("receive_message", data.content);
+    }
+    const newMessage = await Message.create({
+      conversationId: data.conversationId,
+      sender: data.senderId,
+      content:data.content,
+      type:"text",
+    });
+
+    // Update lastMessage in Chat
+    await Conversation.findByIdAndUpdate(data.conversationId, {
+      $set:{"lastMessage.content":data.content,"lastMessage.senderId":data.senderId}
+    });
+    // Emit to all users in the room
+    if(data.participants instanceof Array){
+      io.to(data.conversationId).emit("receive_message", newMessage);
+    }else{
+      io.to(data.participants.userId).emit("receive_message", newMessage)
+    }
+  });
 });
