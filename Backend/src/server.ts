1419555b657c47a18b505b645ad5387fc35e5a4f
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-09-10 09:33:11 +0530

    added CORS To the backend correctly

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -25,10 +25,7 @@ connectDB(process.env.MONGO_URI!);
 // Middleware
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? ["https://gufta-gu.vercel.app"]
-        : ["http://localhost:5173"],
+    origin:["https://gufta-gu.vercel.app","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
